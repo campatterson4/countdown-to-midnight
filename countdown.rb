89c6fb342567429cd81fe111c3f10a9690f419f3
Camryn <camrynpatterson@yahoo.com>
2020-11-16 13:49:06 -0600

    commit tree 1d0500dde4814b9d1b08637db2b40d6edff4cbe6

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
-end
+def countdown(num)
+    while num > 0
+      puts "#{num} SECOND(S)!"
+      sleep 1
+      num -= 1
+    end
+    return "HAPPY NEW YEAR!"
+    end
+    
+    def countdown_with_sleep(num)
+      while num > 0
+      puts "#{num} SECOND(S)!"
+      sleep 1
+      num -= 1
+    end
+    return "HAPPY NEW YEAR!"
+    end
+    
